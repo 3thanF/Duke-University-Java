e0b886a35931a64218961689f5712c9d78193caf
3thanF <3thanfaso03@gmail.com>
2024-02-20 11:00:19 -0600

    Added main method.

--- Shapes.java
+++ Shapes.java
@@ -1,5 +1,9 @@
 public class Shapes{ // Class to measure areas
-    int x = 4;
-    int y = x + 2;
-    int z = y - x;
+    public static void main(String[] args) {
+        int x = 2;
+        int y = x * 3;
+        int z = y / 2;
+        x = (2+z)%2;
+        System.out.println(x);
+    }
 }
