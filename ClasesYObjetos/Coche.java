2e26e8c31162bb22259fe02f87f00393c0788c24
3thanF <3thanfaso03@gmail.com>
2024-02-21 16:48:52 -0600

    POO

--- ClasesYObjetos/Coche.java
+++ ClasesYObjetos/Coche.java
@@ -13,9 +13,18 @@ public class Coche {
         coche1.color = "Amarillo";
         coche1.marca = "Audi";
         coche1.km = 0;
-
+        //Imprimiendo los valores
         System.out.println("El color del coche1 es: " + coche1.color);
         System.out.println("La marca del coche1 es: " + coche1.marca);
-        System.out.print("El kilometraje del coche1 es: " + coche1.km);
+        System.out.println("El kilometraje del coche1 es: " + coche1.km);
+        //Segundo objeto
+        Coche coche2 = new Coche();
+        coche2.color = "Rojo";
+        coche2.marca = "Ferrari";
+        coche2.km = 100;
+
+        System.out.println("El color del coche 2 es: " + coche2.color);
+        System.out.println("La marca del coche 2 es: " + coche2.marca);
+        System.out.println("El kilometraje del coche 2 es: " + coche2.km);
     }
 }
